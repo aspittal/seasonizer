df398e0103e941ffdac84da35e4e63ef4b9030c1
Andrew Spittal <aspittal@gmail.com>
2012-03-13 14:20:41 -0600

    New App Created

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Seasonizer::Application.config.secret_token = '0609b39fa5f3cd2cfb36a1f8ccf7090449e8586567880c079adf7841b4d4e1b0a56258c8c5f52dfb76814087667af5375653692a68f095ff9e2473ac516982f5'
